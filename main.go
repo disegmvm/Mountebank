03eaf7a64f9ab319a323ddfeb7bd2f7a5a08b731
denispeganovri <denis.peganov@river-island.com>
2023-01-18 20:29:16 +0100

    add MBank

--- main.go
+++ main.go
@@ -4,25 +4,12 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"go/helpers"
+	"log"
 	"net/http"
 	"strings"
 )
 
 // album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
 type car struct {
 	ID    string `json:"id"`
 	Title string `json:"title"`
@@ -37,14 +24,13 @@ var cars = []car{
 func main() {
 	router := gin.Default()
 	router.GET("/cars", getCars)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/cars", postCars)
+	router.GET("/cars/:id", getCarByID)
+	router.POST("/cars", post2)
 
 	router.Run("localhost:8080")
 }
 
 func postCars(c *gin.Context) {
-
 	var newCar car
 	if err := c.BindJSON(&newCar); err != nil {
 		return
@@ -68,50 +54,23 @@ func postCars(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, gin.H{"message": mes})
 }
 
-// postAlbumsX responds with the list of all albums as JSON.
-func postAlbumsX(c *gin.Context) {
-
-	var newAlbum album
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-	albums = append(albums, newAlbum)
-	jsonchik := helpers.ToJSON(newAlbum)
-
-	baseUrl := "api-url.com"
-	path := "/v1"
-	aHttpHelper := helpers.NewHttpHelper(baseUrl)
-
-	_, _ = aHttpHelper.Send(
-		aHttpHelper.POST(path).
-			WithContentTypeHeaderJSON().
-			WithBody(strings.NewReader(jsonchik)))
-	//Expect(httpErr).NotTo(HaveOccurred())
-	//Expect(resp.StatusCode).To(Equal(http.StatusCreated))
-
-	mes := fmt.Sprintf("POST is made to %s%s", baseUrl, path)
-
-	c.IndentedJSON(http.StatusCreated, gin.H{"message": mes})
-}
-
 // postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-	var newAlbum album
+func post2(c *gin.Context) {
+	var newCar car
+	if err := c.BindJSON(&newCar); err != nil {
+		return
+	}
+	cars = append(cars, newCar)
+	jsonchik := helpers.ToJSON(newCar)
 
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
+	resp, err := http.Post("http://localhost:4545/test", "application/json", strings.NewReader(jsonchik))
+
+	if err != nil {
+		log.Printf("Request Failed: %s", err)
 		return
 	}
 
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+	log.Printf("SENT SUCCESSFULLY!!!!!!!: %s", resp.StatusCode)
 }
 
 // getCars responds with the list of all albums as JSON.
@@ -119,14 +78,14 @@ func getCars(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, cars)
 }
 
-// getAlbumByID locates the album whose ID value matches the id
+// getCarByID locates the album whose ID value matches the id
 // parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
+func getCarByID(c *gin.Context) {
 	id := c.Param("id")
 
 	// Loop through the list of albums, looking for
 	// an album whose ID value matches the parameter.
-	for _, a := range albums {
+	for _, a := range cars {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
 			return
